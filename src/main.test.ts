83f38498b5aafb6eb74c5cf0c76396fc3a490019
Luis <luisirk.montoto@gmail.com>
2024-08-21 19:23:29 +0200

    Green: return 0 and 1

--- src/main.test.ts
+++ src/main.test.ts
@@ -6,3 +6,23 @@ describe("Default test", () => {
     expect(main.getParam()).toBe("Hello world");
   });
 });
+
+describe("String Calculator", ()=>{
+    function add(number: string): string {
+        if(number ===''){
+            return '0';
+        }
+        return '1';
+
+
+
+    }
+    // ''  -> 0
+    it ('returns zero when param is empty', ()=>{
+        expect(add('')).toBe('0')
+    });
+    // '1' => 1
+    it('returns 1 when param is 1', ()=>{
+        expect(add('1')).toBe('1')
+    })
+})
