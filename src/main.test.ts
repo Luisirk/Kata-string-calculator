c5279142cca6587313b300d62d0d8f9be44193ff
Luis <luisirk.montoto@gmail.com>
2024-08-26 12:56:20 +0200

    Green: returns the sum when provide multiple numbers at expression

--- src/main.test.ts
+++ src/main.test.ts
@@ -12,11 +12,26 @@ function add(expression:string):string {
     if (expression === ''){
         return '0';
     }
+    let i = 0;
+    const number = []
 
 
+    while (i < expression.length) {
+        let separatorPosition = expression.indexOf(',', i)
+        if (separatorPosition === -1) {
+            separatorPosition = expression.length
+        }
+        const firstNumber = expression.substring(i, separatorPosition)
+        number.push(parseInt(firstNumber));
+        i = separatorPosition + 1;
+    }
+    let result:number=0;
+    for (i = 0; i < number.length; i++) {
+        result += number[i];
+    }
 
 
-    return expression;
+    return result.toString();
 }
 
 
@@ -25,22 +40,21 @@ function add(expression:string):string {
 
 describe("String Calculator", ()=> {
     // ''-> return 0
-    it ('returns zero when expression is empty', ()=> {
-        expect(add('')).toBe('0')}
-    )})
+    it('returns zero when expression is empty', () => {
+        expect(add('')).toBe('0')
+    })
 //'1' =>1
 
-    it('returns the number when has only one number',()=>{
+    it('returns the number when has only one number', () => {
         expect(add('1')).toBe('1')
         expect(add('10')).toBe('10')
         expect(add('100')).toBe('100')
     })
 //'1,1 => 2
 
-it('returns the number when has only one number',()=>{
-    expect(add('1,1')).toBe('2')
-    expect(add('1,10')).toBe('11')
+    it('returns the sum when provide multiple numbers at expression', () => {
+        expect(add('1,1')).toBe('2')
+        expect(add('1,10')).toBe('11')
+    })
 })
-
-
 
