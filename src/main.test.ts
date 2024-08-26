42eba0a970c1136328dfc5cd5b491e2c33868dc7
Luis <luisirk.montoto@gmail.com>
2024-08-26 18:43:03 +0200

    Green: returns zero when expression is empty

--- src/main.test.ts
+++ src/main.test.ts
@@ -9,29 +9,11 @@ describe("Default test", () => {
 
 
 function add(expression:string):string {
-    if (expression === ''){
-        return '0';
+    // throw new Error('Not implemented yet');
+    if(expression === '') {
+        return '0'
     }
-    let i = 0;
-    const number = []
-
-
-    while (i < expression.length) {
-        let separatorPosition = expression.indexOf(',', i)
-        if (separatorPosition === -1) {
-            separatorPosition = expression.length
-        }
-        const firstNumber = expression.substring(i, separatorPosition)
-        number.push(parseInt(firstNumber));
-        i = separatorPosition + 1;
-    }
-    let result:number=0;
-    for (i = 0; i < number.length; i++) {
-        result += number[i];
-    }
-
-
-    return result.toString();
+    return expression;
 }
 
 
@@ -45,16 +27,6 @@ describe("String Calculator", ()=> {
     })
 //'1' =>1
 
-    it('returns the number when has only one number', () => {
-        expect(add('1')).toBe('1')
-        expect(add('10')).toBe('10')
-        expect(add('100')).toBe('100')
-    })
-//'1,1 => 2
 
-    it('returns the sum when provide multiple numbers at expression', () => {
-        expect(add('1,1')).toBe('2')
-        expect(add('1,10')).toBe('11')
-    })
 })
 
