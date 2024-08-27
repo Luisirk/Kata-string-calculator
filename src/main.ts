32098a7e73193dedea4ca800eb336507e8586332
Adrián Ferrera <adrian.ferrera@leanmind.es>
2024-08-27 10:33:01 +0100

    Green: returns sum of two numbers when expression is two numbers separated by comma

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,10 @@ export function add(expression: string): string {
   if (expression === '') {
     return '0';
   }
+  if(expression.includes(',')) {
+    const firstNumber = expression[0]
+    const secondNumber = expression[2]
+    return (parseInt(firstNumber) + parseInt(secondNumber)).toString();
+  }
   return expression;
 }
