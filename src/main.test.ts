7bf35c079e18fe4b94dc02bfe54a3988b88347b0
Adrián Ferrera <adrian.ferrera@leanmind.es>
2024-08-27 10:31:45 +0100

    Red: returns sum of two numbers when expression is two numbers separated by comma

--- src/main.test.ts
+++ src/main.test.ts
@@ -15,6 +15,11 @@ describe("String Calculator", () => {
     expect(add("2")).toBe('2');
   });
 
+  it("returns sum of two numbers when expression is two numbers separated by comma", () => {
+    expect(add("1,2")).toBe('3');
+    // expect(add("2,3")).toBe('5');
+  });
+
 
 });
 
