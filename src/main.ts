b3c4e3e9a9618e847106f2d9617c7f7cfaf96e21
Adrián Ferrera <adrian.ferrera@leanmind.es>
2024-08-29 10:45:35 +0100

    Refactor: Extract function to split number from expression

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,25 @@
 
-
+function splitNextNumberFromExpression(expression: string): [number, string] {
+  const firstCommaIndex = expression.indexOf(',', 0);
+  if (firstCommaIndex !== -1) {
+    const number = parseInt(expression.substring(0, firstCommaIndex));
+    const rest = expression.substring(firstCommaIndex + 1);
+    return [number, rest];
+  }
+  return [parseInt(expression), ''];
+}
 
 export function add(expression: string): string {
   if (expression === '') {
     return '0';
   }
   if(expression.includes(',')) {
-    const firstCommaIndex = expression.indexOf(',');
-    let sumOfNumbers = parseInt(expression.substring(0, firstCommaIndex));
-
-    if (firstCommaIndex !== -1) {
-      const secondNumber = expression.substring(firstCommaIndex + 1);
-      sumOfNumbers += parseInt(secondNumber);
-
-      const secondCommaIndex = expression.indexOf(',', firstCommaIndex + 1);
-      if(secondCommaIndex !== -1) {
-        const thirdNumber = expression.substring(secondCommaIndex + 1);
-        sumOfNumbers += parseInt(thirdNumber);
-
-        const thirdCommaIndex = expression.indexOf(',', secondCommaIndex + 1);
-        if(thirdCommaIndex !== -1) {
-          const fourthNumber = expression.substring(thirdCommaIndex + 1);
-          sumOfNumbers += parseInt(fourthNumber);
-        }
-
-      }
+    let sumOfNumbers = 0;
+    let rest = expression;
+    while (rest !== '') {
+      const result = splitNextNumberFromExpression(rest);
+      sumOfNumbers += result[0];
+      rest = result[1];
     }
     return sumOfNumbers.toString();
   }
