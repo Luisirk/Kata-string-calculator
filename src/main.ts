6a0c5e808fec62318a288a9c2b7e33e496bc10e4
Luis <luisirk.montoto@gmail.com>
2024-09-11 11:51:42 +0200

    Green: Returns sum of number when it has a custom separator

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 function splitNextNumberFromExpression(expression: string): [number, string] {
   const firstCommaIndex = expression.indexOf(',', 0);
+
+
   if (firstCommaIndex !== -1) {
     const number = parseInt(expression.substring(0, firstCommaIndex));
     const rest = expression.substring(firstCommaIndex + 1);
@@ -18,10 +20,22 @@ function sumNumbersSplitByComma(expression: string) {
   }
   return sumOfNumbers.toString();
 }
-
+//Lo primero que vamos a hacer es separar el separador personalizado de los numeros
+//Reemplazar en los numeros el separar personalizado por la coma
+//(Para evitar reciclar el resto del codigo)
 export function add(expression: string): string {
   if (expression === '') return '0';
-  if (!expression.includes(',')) return expression;
-  return sumNumbersSplitByComma(expression);
+  let evaluateExpression = expression;
+
+  if (evaluateExpression.startsWith('//')) {
+    const jumpPosition = evaluateExpression.indexOf('\n')
+   // const customSeparator = evaluateExpression.substring('//'.length, jumpPosition);
+    const customSeparator = ';'
+    const listOfNumbers = evaluateExpression.substring(jumpPosition + 1);
+    evaluateExpression = listOfNumbers.replace(customSeparator,',')
+
+  }
+  if (!evaluateExpression.includes(',')) return evaluateExpression;
+  return sumNumbersSplitByComma(evaluateExpression);
 }
 
